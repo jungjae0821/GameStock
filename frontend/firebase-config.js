10671a0f90018e2f1d2d4d5a92fb7b118644f4e3
Park JungJae <wizjungjae2@gmail.com>
2026-09-09 12:53:37 +0900

    firebase 연결

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -1,3 +1,9 @@
-// Firebase 콘솔의 프로젝트 설정값을 아래에 붙여 넣으세요.
-// 이 값들은 브라우저에 공개되어도 되는 식별자이며, 서비스 계정 키는 절대 넣지 마세요.
-window.GAMESTOCK_FIREBASE_CONFIG = null;
+// Firebase 웹 앱 설정입니다. 기존 compat SDK가 읽을 수 있도록 전역으로 노출합니다.
+window.GAMESTOCK_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAzUEj5xwH14p3BBj7RwJ92tj9eoM9cuKI",
+  authDomain: "gamestock-20994.firebaseapp.com",
+  projectId: "gamestock-20994",
+  storageBucket: "gamestock-20994.firebasestorage.app",
+  messagingSenderId: "957680390567",
+  appId: "1:957680390567:web:8f0d60ea888daae68e2682",
+};
